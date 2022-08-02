017eec2c020b74ee242bcd242043f6543fb0f7c4
Bruna Tomás Maiochi <btomasmaiochi@gmail.com>
2022-08-02 20:28:50 -0300

    slider part 3

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const previousEl = document.getElementById("previous");
 const nextEl = document.getElementById("next");
 const sliderEl = document.getElementById("slider");
+let interval = undefined;
+let timeout = undefined;
+let selectedImgIndex = 0;
 
 previousEl.addEventListener("click", onPreviousClick);
 nextEl.addEventListener("click", onNextClick);
@@ -10,28 +13,57 @@ autoScroll();
 function onPreviousClick() {
 	const sliderWidth = sliderEl.offsetWidth;
 	sliderEl.scrollLeft -= sliderWidth;
+	--selectedImgIndex;
+	handleActiveDot();
 	handleSliderClick();
 }
 
 function onNextClick() {
 	const sliderWidth = sliderEl.offsetWidth;
 	sliderEl.scrollLeft += sliderWidth;
+	++selectedImgIndex;
+	handleActiveDot();
 	handleSliderClick();
 }
 
+function handleSliderClick() {
+	clearTimeout(timeout);
+	clearInterval(interval);
+	interval = undefined;
+	timeout = setTimeout(() => {
+		autoScroll();
+	}, 10000);
+}
+
+function handleActiveDot() {
+	const list = Array.from(document.getElementsByClassName("dot"));
+
+	if (selectedImgIndex < 0) selectedImgIndex = 0;
+	if (selectedImgIndex >= list.length) selectedImgIndex = list.length - 1;
+
+	list.forEach((el) => el.classList.remove("active"));
+	list[selectedImgIndex].classList.add("active");
+}
+
 function autoScroll() {
-	setInterval(() => {
+	if (interval) return;
+
+	interval = setInterval(() => {
 		const sliderWidth = sliderEl.offsetWidth;
 		const numberOfImages = sliderEl.childElementCount;
-		const selectedImages = sliderEl.scrollLeft / sliderWidth + 1;
+		const selectedImage = sliderEl.scrollLeft / sliderWidth + 1;
 
 		// Si es la última => vuelve al 0
-		if (numberOfImages === selectedImages) {
+		if (numberOfImages === selectedImage) {
 			sliderEl.scrollLeft = 0;
+			selectedImgIndex = 0;
+			handleActiveDot();
 			return;
 		}
 
 		//Sino scrollea a la derecha cada 5s
 		sliderEl.scrollLeft += sliderWidth;
+		++selectedImgIndex;
+		handleActiveDot();
 	}, 5000);
 }
