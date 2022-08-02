c9b71b8ddd80fc1202e3b4045e8825c6af5121f2
Bruna Tomás Maiochi <btomasmaiochi@gmail.com>
2022-08-02 18:38:24 -0300

    slider part 2

--- index.js
+++ index.js
@@ -5,12 +5,33 @@ const sliderEl = document.getElementById("slider");
 previousEl.addEventListener("click", onPreviousClick);
 nextEl.addEventListener("click", onNextClick);
 
+autoScroll();
+
 function onPreviousClick() {
 	const sliderWidth = sliderEl.offsetWidth;
 	sliderEl.scrollLeft -= sliderWidth;
+	handleSliderClick();
 }
 
 function onNextClick() {
 	const sliderWidth = sliderEl.offsetWidth;
 	sliderEl.scrollLeft += sliderWidth;
+	handleSliderClick();
+}
+
+function autoScroll() {
+	setInterval(() => {
+		const sliderWidth = sliderEl.offsetWidth;
+		const numberOfImages = sliderEl.childElementCount;
+		const selectedImages = sliderEl.scrollLeft / sliderWidth + 1;
+
+		// Si es la última => vuelve al 0
+		if (numberOfImages === selectedImages) {
+			sliderEl.scrollLeft = 0;
+			return;
+		}
+
+		//Sino scrollea a la derecha cada 5s
+		sliderEl.scrollLeft += sliderWidth;
+	}, 5000);
 }
